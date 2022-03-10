493fd45fc17c7f55f167b1d9d204fa4f83b92588
Siwon <73284904+tldnjs324@users.noreply.github.com>
2022-03-10 23:40:00 +0900

    cubecake

---
+++ Assets/Scripts/CubeCake/CameraManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera CameraR;
+    public Camera CameraL;
+    public Camera CameraF;
+    public Camera CameraA;
+
+    public Camera CorrectCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraA.click
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CorrectAnswer()
+    {
+
+    }
+}
