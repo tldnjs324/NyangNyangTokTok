38e216ab7b2dee92fa4189af5e1e7a2055de3c2b
Siwon <73284904+tldnjs324@users.noreply.github.com>
2022-03-15 23:31:42 +0900

    random question
    랜덤 질문 설정

--- Assets/Scripts/CubeCake/AnswerCtrl.cs
+++ Assets/Scripts/CubeCake/AnswerCtrl.cs
@@ -5,17 +5,17 @@ using UnityEngine.UI;
 
 public class AnswerCtrl : MonoBehaviour
 {
+    //질문 어디서 본 모양을 고르세요
     public Text question;
-    
-    //public Camera CameraR;
-    //public Camera CameraF;
-    //public Camera CameraA;
-    //public Camera CameraL;
-    
+    //질문 지정을 위한 숫자
     private int random;
 
     //카메라 리스트(오앞위뒤)
     public List<Camera> CameraList = new List<Camera>();
+    //버튼 리스트(오앞위뒤)
+    public List<Button> ButtonList = new List<Button>();
+    //틀렸을 때 가려지는 회색 이미지 리스트(오앞위뒤)
+    public List<Image> NoneList = new List<Image>();
 
     //카메라 위치 리스트
     public List<Rect> RectList = new List<Rect>() {
@@ -24,12 +24,6 @@ public class AnswerCtrl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //CameraList.Add(CameraR);
-        //CameraList.Add(CameraF);
-        //CameraList.Add(CameraA);
-        //CameraList.Add(CameraL);
-
-
         for (int i = 0; i<4; i++)
         {
             int rand = Random.Range(0, RectList.Count);
@@ -37,6 +31,7 @@ public class AnswerCtrl : MonoBehaviour
             RectList.RemoveAt(rand);
         }
         random = Random.Range(0, 4);
+        RandAnswer();
     }
 
     // Update is called once per frame
@@ -45,11 +40,22 @@ public class AnswerCtrl : MonoBehaviour
         
     }
 
-    public void Randrfal()
+    public void RandAnswer()
     {
         if(random == 0)
         {
-
+            question.text = "앞에서 본 모양을 고르세요.";
+        }else if(random == 1)
+        {
+            question.text = "왼쪽에서 본 모양을 고르세요.";
+        }
+        else if (random == 2)
+        {
+            question.text = "오른쪽에서 본 모양을 고르세요.";
+        }
+        else if (random == 3)
+        {
+            question.text = "위에서 본 모양을 고르세요.";
         }
     }
 }
