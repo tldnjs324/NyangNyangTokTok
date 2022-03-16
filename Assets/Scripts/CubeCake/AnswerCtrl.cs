f062ab815b6382d6a14b2e1712a93ac038d24e72
Siwon <73284904+tldnjs324@users.noreply.github.com>
2022-03-16 10:42:39 +0900

    cubecake random answer
    큐브케이크 랜덤 정답 지정 (기술적으로는 완료)

--- Assets/Scripts/CubeCake/AnswerCtrl.cs
+++ Assets/Scripts/CubeCake/AnswerCtrl.cs
@@ -10,20 +10,28 @@ public class AnswerCtrl : MonoBehaviour
     //질문 지정을 위한 숫자
     private int random;
 
-    //카메라 리스트(오앞위뒤)
+    //카메라 리스트(오앞위왼)
     public List<Camera> CameraList = new List<Camera>();
-    //버튼 리스트(오앞위뒤)
+    //버튼 리스트(오앞위왼)
     public List<Button> ButtonList = new List<Button>();
-    //틀렸을 때 가려지는 회색 이미지 리스트(오앞위뒤)
+    //틀렸을 때 가려지는 회색 이미지 리스트(오앞위왼)
     public List<Image> NoneList = new List<Image>();
 
-    //카메라 위치 리스트
+    //카메라 위치 리스트(카메라 위치 랜덤 설정 할 때 사용)
     public List<Rect> RectList = new List<Rect>() {
         new Rect(0.5f, 0.5f, 0.25f, 0.5f), new Rect(0.5f, 0, 0.25f, 0.5f), new Rect(0.75f, 0, 0.25f, 0.5f), new Rect(0.75f, 0.5f, 0.25f, 0.5f)};
+
+    //맞았을 시 팝업
+    public GameObject sign_yes;
+    //틀렸을 시 팝업
+    public GameObject sign_no;
+    //팝업 뜰 때 검은 배경 화면
+    public GameObject black_screen;
 
     // Start is called before the first frame update
     void Start()
     {
+        //카메라 위치 랜덤 설정
         for (int i = 0; i<4; i++)
         {
             int rand = Random.Range(0, RectList.Count);
@@ -40,22 +48,111 @@ public class AnswerCtrl : MonoBehaviour
         
     }
 
+    //정답 지정( 및 버튼 및 회색 이미지 지정)
     public void RandAnswer()
     {
         if(random == 0)
         {
-            question.text = "앞에서 본 모양을 고르세요.";
+            question.text = "오른쪽에서 본 모양을 고르세요.";
         }else if(random == 1)
         {
-            question.text = "왼쪽에서 본 모양을 고르세요.";
+            question.text = "앞쪽에서 본 모양을 고르세요.";
         }
         else if (random == 2)
         {
-            question.text = "오른쪽에서 본 모양을 고르세요.";
+            question.text = "위쪽에서 본 모양을 고르세요.";
         }
         else if (random == 3)
         {
-            question.text = "위에서 본 모양을 고르세요.";
+            question.text = "왼쪽에서 본 모양을 고르세요.";
         }
+    }
+
+    public void Btn0Clicked()
+    {
+        for(int i = 0; i<4; i++)
+        {
+            if (CameraList[i].rect == new Rect(0.5f, 0.5f, 0.25f, 0.5f))
+            {
+                if(i == random)
+                {
+                    sign_yes.SetActive(true);
+                    black_screen.SetActive(true);
+                }
+                else
+                {
+                    sign_no.SetActive(true);
+                    black_screen.SetActive(true);
+                }
+            }
+        }
+    }
+    public void Btn1Clicked()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (CameraList[i].rect == new Rect(0.75f, 0.5f, 0.25f, 0.5f))
+            {
+                if (i == random)
+                {
+                    sign_yes.SetActive(true);
+                    black_screen.SetActive(true);
+                }
+                else
+                {
+                    sign_no.SetActive(true);
+                    black_screen.SetActive(true);
+                }
+            }
+        }
+    }
+    public void Btn2Clicked()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (CameraList[i].rect == new Rect(0.5f, 0, 0.25f, 0.5f))
+            {
+                if (i == random)
+                {
+                    sign_yes.SetActive(true);
+                    black_screen.SetActive(true);
+                }
+                else
+                {
+                    sign_no.SetActive(true);
+                    black_screen.SetActive(true);
+                }
+            }
+        }
+    }
+    public void Btn3Clicked()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (CameraList[i].rect == new Rect(0.75f, 0, 0.25f, 0.5f))
+            {
+                if (i == random)
+                {
+                    sign_yes.SetActive(true);
+                    black_screen.SetActive(true);
+                }
+                else
+                {
+                    sign_no.SetActive(true);
+                    black_screen.SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void Close_No()
+    {
+        sign_no.SetActive(false);
+        black_screen.SetActive(false);
+    }
+
+    public void NextScene()
+    {
+        //씬 넘어가는 코드
     }
 }
