b5418b90b67492fb9150187744c54b94f6050ac1
SeoyoonPark <tjdbs118@gmail.com>
2022-03-31 04:32:12 +0900

    ordermenu1 연결

--- Assets/Scripts/Calculator.cs
+++ Assets/Scripts/Calculator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class Calculator : MonoBehaviour
 {
@@ -61,5 +62,66 @@ public class Calculator : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void NextBtn()
+    {
+        //audioSrc.PlayOneShot(click, 0.5f);
+        if (GameManager.OrderMenu1 == "따뜻한 아메리카노")
+        {
+            SceneManager.LoadScene("HotAmericano");
+        }
+        else if (GameManager.OrderMenu1 == "아이스 아메리카노")
+        {
+            SceneManager.LoadScene("IceAmericano");
+        }
+        else if (GameManager.OrderMenu1 == "따뜻한 카페라떼")
+        {
+            SceneManager.LoadScene("HotLatte");
+        }
+        else if (GameManager.OrderMenu1 == "아이스 카페라떼")
+        {
+            SceneManager.LoadScene("IceLatte");
+        }
+        else if (GameManager.OrderMenu1 == "따뜻한 바닐라라떼")
+        {
+            SceneManager.LoadScene("HotVanillaLatte");
+        }
+        else if (GameManager.OrderMenu1 == "아이스 바닐라라떼")
+        {
+            SceneManager.LoadScene("IceVanillaLatte");
+        }
+        else if (GameManager.OrderMenu1 == "따뜻한 카페모카")
+        {
+            SceneManager.LoadScene("HotCafeMocha");
+        }
+        else if (GameManager.OrderMenu1 == "아이스 카페모카")
+        {
+            SceneManager.LoadScene("IceCafeMocha");
+        }
+        else if (GameManager.OrderMenu1 == "기본 토스트")
+        {
+            SceneManager.LoadScene("BasicToast");
+        }
+        else if (GameManager.OrderMenu1 == "초코 토스트")//
+        {
+            SceneManager.LoadScene("IceAmericano");
+        }
+        else if (GameManager.OrderMenu1 == "딸기 토스트")
+        {
+            SceneManager.LoadScene("IceAmericano");
+        }
+        else if (GameManager.OrderMenu1 == "블루베리 토스트")
+        {
+            SceneManager.LoadScene("IceAmericano");
+        }
+        else if (GameManager.OrderMenu1 == "딸기 블루베리 토스트")
+        {
+            SceneManager.LoadScene("IceAmericano");
+        }
+        else if (GameManager.OrderMenu1 == "냥냥 토스트")
+        {
+            SceneManager.LoadScene("IceAmericano");
+        }
     }
 }
