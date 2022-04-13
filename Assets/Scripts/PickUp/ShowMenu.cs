8fa1dfcf32f7aa8e319934716597ef731ddf5356
Siwon <73284904+tldnjs324@users.noreply.github.com>
2022-04-14 02:20:37 +0900

    Show Menu

--- Assets/Scripts/PickUp/ShowMenu.cs
+++ Assets/Scripts/PickUp/ShowMenu.cs
@@ -7,22 +7,39 @@ public class ShowMenu : MonoBehaviour
     public GameObject[] Menu;
     public GameObject[] SelectedMenu;
 
+    public List<Vector3> points = new List<Vector3>();
 
     // Start is called before the first frame update
     void Start()
     {
-        SelectedMenu[0].transform.position = new Vector3(-6, 0, 0);
-        SelectedMenu[1].transform.position = new Vector3(-2, 0, 0);
-        SelectedMenu[3].transform.position = new Vector3(2, 0, 0);
-        SelectedMenu[4].transform.position = new Vector3(6, 0, 0);
+        if (SpecifyNumber.MakingMenu[2] == 20)//메뉴가 2개일 때
+        {
+            points.Add(new Vector3(-2, 0, 0));
+            points.Add(new Vector3(2, 0, 0));
+        }else if(SpecifyNumber.MakingMenu[2]<20 && SpecifyNumber.MakingMenu[3] == 20)//메뉴가 3개일 때
+        {
+            points.Add(new Vector3(-4, 0, 0));
+            points.Add(new Vector3(0, 0, 0));
+            points.Add(new Vector3(4, 0, 0));
+        }
+        else//메뉴가 4개일 때
+        {
+            points.Add(new Vector3(-6, 0, 0));
+            points.Add(new Vector3(-2, 0, 0));
+            points.Add(new Vector3(2, 0, 0));
+            points.Add(new Vector3(6, 0, 0));
+        }
+
+        
+
         for (int i = 0; i<4; i++)
         {
             for(int j = 0; j<20; j++)
             {
                 if(SpecifyNumber.MakingMenu[i] == j)
                 {
-                    SelectedMenu[i] = Menu[j];
-                    SelectedMenu[i].SetActive(true);
+                    Menu[j].transform.position = points[i];
+                    Menu[j].SetActive(true);
                 }
                 
             }
