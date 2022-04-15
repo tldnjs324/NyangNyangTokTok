7aa4331f9a84c1e2b6bd4e75ced783ee7a812100
SeoyoonPark <tjdbs118@gmail.com>
2022-04-16 03:33:26 +0900

    tutorial_ordercoffee

---
+++ Assets/Scripts/Tutorial/OrderHelp1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrderHelp1 : MonoBehaviour
+{
+    public GameObject popupHelp;
+    public Text helpText;
+
+    string hint = "";
+    string hint1;
+    string hint2;
+
+    void Start()
+    {
+        hint1 = "ししさ しけぉせい";
+        hint2 = "3ぁ せげせしせ";
+
+        
+    }
+}
