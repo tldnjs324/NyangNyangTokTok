650eb9538a4bfc22b1f98a26f4f338879822b716
SeoyoonPark <tjdbs118@gmail.com>
2022-05-17 05:39:59 +0900

    popup_6
    큐브완

--- Assets/Scripts/PickUp/StartPopup.cs
+++ Assets/Scripts/PickUp/StartPopup.cs
@@ -5,24 +5,29 @@ using UnityEngine;
 public class StartPopup : MonoBehaviour
 {
     //²¨³» º¾½Ã´Ù ÆË¾÷
-    public GameObject BringUpPopup;
+    //public GameObject BringUpPopup;
     //¼³¸íÇØÁÖ´Â ÆË¾÷
-    public GameObject ExplainPopup;
-
+    //public GameObject ExplainPopup;
+    public GameObject popupStart;
 
     public void ShowExplainPopup()
     {
-        BringUpPopup.SetActive(false);
-        ExplainPopup.SetActive(true);
+        //BringUpPopup.SetActive(false);
+        //ExplainPopup.SetActive(true);
     }
     public void CloseExplainPopup()
     {
-        ExplainPopup.SetActive(false);
+        //ExplainPopup.SetActive(false);
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("ShowStartPopup", 0.5f);
+    }
+    void ShowStartPopup()
+    {
+        Popup pop = popupStart.GetComponent<Popup>();
+        pop.PopUp2();
     }
 
     // Update is called once per frame
